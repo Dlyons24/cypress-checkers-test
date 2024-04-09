14e82703b80e921f016bbb3dc1c86919dc40e2f6
Dlyons24 <96287175+Dlyons24@users.noreply.github.com>
2024-04-09 00:59:13 -0400

    Update move_checkers.spec.js

--- move_checkers.spec.js
+++ move_checkers.spec.js
@@ -3,15 +3,12 @@ describe('Checkers Game Test', () => {
       // Visit the game website
       cy.visit('https://www.gamesforthebrain.com/game/checkers/');
   
-      // You might need to adjust the selectors based on the game's structure
-      // Example: Select the orange checker piece
+      // Select orange checker piece
       cy.get('img[name="space02"]').click();
   
-      // Then click on the destination space
-      cy.get('img[name="destination-space-name"]').click();
+      // click on the destination space
+      cy.get('img[name="space13"]').click();
   
-      // Add assertions here to verify the move was successful
-      // For example, you could check if the destination space now contains an orange piece
-      // and the original space does not
+     
     });
   });
