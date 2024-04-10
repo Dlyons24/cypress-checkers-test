c31b0af7818a0d802ded60e87c6ca513c31e273b
Dlyons24 <donovan.lyons@calendly.com>
2024-04-09 22:15:33 -0400

    Checker game finished

--- move_checkers.spec.js
+++ move_checkers.spec.js
@@ -8,13 +8,28 @@ describe('Checkers Game Test', () => {
   
       // click on the destination space
       cy.get('img[name="space13"]').click();
+
       
       cy.get('img[name="space02"]').click(); 
-     
+
+      
       cy.get('img[name="space04"]').click();
-    
-    
-    
+
+      
+      cy.get('img[name="space15"]').click();
+      cy.get('#message').should('have.text', 'Make a move.');
+
+      cy.get('img[name="space22"]').click();
+      cy.get('img[name="space33"]').click();
+      cy.get('#message').should('have.text', 'Make a move.');
+
+      cy.get('img[name="space15"]').click();
+      cy.get('#message').should('have.text', 'Make a move.');
+
+      cy.contains('a', 'Restart...').click();
+
+      // This text will be validating that the game has restarted 
+      cy.get('#message').should('have.text', 'Select an orange piece to move.');
     });
 
   });
